1db2ac99e3a2606c3414a5ba80f1bb7a6047c141
HigorLP <higor.leandro@eb.mil.br>
2022-10-13 16:05:54 -0300

    Project Completed

--- README.md
+++ README.md
@@ -23,3 +23,6 @@ https://drive.google.com/file/d/1RPevwj4hdsUsyZff-i5nQNaLONO-HLax/view?usp=shari
 Finalização do HTML e CSS, Início do Script.
 
 --
+
+=> 13 de Outubro de 2022 --> Finalização da Aula 03,
+Projeto Finalizado.
--- js/index.js
+++ js/index.js
@@ -7,35 +7,193 @@ const Modal = {
   },
 };
 
-const transactions = [{ id: 1, description: "", amount: 0, date: "" }];
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem("dev.finance:transactions")) || [];
+  },
+  set(transactions) {
+    localStorage.setItem(
+      "dev.finance:transactions",
+      JSON.stringify(transactions)
+    );
+  },
+};
 
-const Transactions = {
+const Transaction = {
+  all: Storage.get(),
+
+  add(transaction) {
+    Transaction.all.push(transaction);
+    App.reload();
+  },
+
+  remove(index) {
+    Transaction.all.splice(index, 1);
+    App.reload();
+  },
+
   incomes() {
-    // add incomes
+    let income = 0;
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount > 0) {
+        income += transaction.amount;
+      }
+    });
+    return income;
   },
+
   expenses() {
-    // add expenses
+    let expense = 0;
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount < 0) {
+        expense += transaction.amount;
+      }
+    });
+    return expense;
   },
+
   total() {
-    // add total
+    return Transaction.incomes() + Transaction.expenses();
   },
 };
 
 const DOM = {
-  addTransaction(transactions, index) {
+  transactionsContainer: document.querySelector("#data-table tbody"),
+
+  addTransaction(transaction, index) {
     const tr = document.createElement("tr");
-    tr.innerHTML = DOM.innerHTMLTransaction();
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
+
+    DOM.transactionsContainer.appendChild(tr);
   },
-  innerHTMLTransaction() {
+
+  innerHTMLTransaction(transaction, index) {
+    const cssClass = transaction.amount > 0 ? "income" : "expense";
+
+    const amount = Utils.formatCurrency(transaction.amount);
+
     const html = `
-        <td class="description"></td> 
-        <td class="expense"></td> 
-        <td class="date"></td> 
+        <td class="description">${transaction.description}</td> 
+        <td class="${cssClass}">${amount}</td> 
+        <td class="date">${transaction.date}</td> 
         <td>
-        <img src="./assets/remove.svg" alt="Remove Transaction" />
+        <img onClick="Transaction.remove(${index})" src="./assets/remove.svg" alt="Remove Transaction"" />
         </td>
         `;
-
     return html;
   },
+
+  updateBalance() {
+    document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.incomes()
+    );
+    document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.expenses()
+    );
+    document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(
+      Transaction.total()
+    );
+  },
+
+  clearTransactions() {
+    DOM.transactionsContainer.innerHTML = "";
+  },
 };
+
+const Utils = {
+  formatAmount(value) {
+    value = Number(value) * 100;
+    return value;
+  },
+
+  formatDate(date) {
+    const splittedDate = date.split("-");
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  },
+
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : "";
+    value = String(value).replace(/\D/g, "");
+    value = Number(value) / 100;
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+    return signal + value;
+  },
+};
+
+const Form = {
+  description: document.querySelector("input#description"),
+  amount: document.querySelector("input#amount"),
+  date: document.querySelector("input#date"),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
+  },
+
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
+    if (
+      description.trim() === "" ||
+      amount.trim() === "" ||
+      date.trim() === ""
+    ) {
+      throw new Error("Preencha todos os Campos!");
+    }
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+    amount = Utils.formatAmount(amount);
+
+    date = Utils.formatDate(date);
+
+    return {
+      description,
+      amount,
+      date,
+    };
+  },
+
+  clearFields() {
+    Form.description.value = "";
+    Form.amount.value = "";
+    Form.date.value = "";
+  },
+
+  submit(event) {
+    event.preventDefault();
+
+    try {
+      Form.validateFields();
+      const transaction = Form.formatValues();
+      Transaction.add(transaction);
+      Form.clearFields();
+      Modal.close();
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+};
+
+const App = {
+  init() {
+    Transaction.all.forEach(DOM.addTransaction);
+
+    DOM.updateBalance();
+
+    Storage.set(Transaction.all);
+  },
+  reload() {
+    DOM.clearTransactions();
+    App.init();
+  },
+};
+
+App.init();
